53c95800c40c5bab65174146a53776540d987c93
github-action-benchmark <github@users.noreply.github.com>
2025-01-14 18:35:29 +0000

    add Benchmark (catch2) benchmark result for c49842718eaa860b46a4028965bd78e5f5fac4ba

---
+++ dev/bench/data.js
@@ -0,0 +1,53 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1736879729220,
+  "repoUrl": "https://github.com/paulmthompson/Whisker-Analysis",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "pmt10@case.edu",
+            "name": "Paul Thompson",
+            "username": "paulmthompson"
+          },
+          "committer": {
+            "email": "pmt10@case.edu",
+            "name": "Paul Thompson",
+            "username": "paulmthompson"
+          },
+          "distinct": true,
+          "id": "c49842718eaa860b46a4028965bd78e5f5fac4ba",
+          "message": "fix typo in repository name",
+          "timestamp": "2025-01-14T13:26:25-05:00",
+          "tree_id": "7f6a58cad2a6b84a95c82cc23504e980ec5cd5d4",
+          "url": "https://github.com/paulmthompson/Whisker-Analysis/commit/c49842718eaa860b46a4028965bd78e5f5fac4ba"
+        },
+        "date": 1736879728583,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "IoU Calculation",
+            "value": 8.03306,
+            "range": "± 577.563",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          },
+          {
+            "name": "IoU Relative Calculation",
+            "value": 7.27627,
+            "range": "± 305.169",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Mask Intersection",
+            "value": 29.2426,
+            "range": "± 1.90047",
+            "unit": "ns",
+            "extra": "100 samples\n1029 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
